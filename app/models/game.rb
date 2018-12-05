66624a3ab2414475e8a6f245f3bf83564fc0f0d1
github username <patcarrasco@outlook.com>
2018-12-05 11:02:54 -0500

    added functions to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,6 +9,7 @@ class Game < ActiveRecord::Base
     question_ids.each do |id|
       GameQuestion.create(game_id: game.id, question_id: id)
     end
+    return game
   end
 
   def self.game_make_by_difficulty(user_id, question_no, difficulty)
@@ -23,6 +24,18 @@ class Game < ActiveRecord::Base
   def score
     amt_right = questions_in_current_game.collect{|gq| gq.correct?}.count(true)
     amt_right.to_f / questions_in_current_game.size
+  end
+
+  def current_question(current_id)
+    questions_in_current_game.select{|gq| gq.id == current_id}
+  end
+
+  def right_answer(question_id)
+    current_question.correct?
+  end
+
+  def wrong_answer(question_id)
+    current_question.correct?
   end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -8,4 +8,5 @@ class Question < ActiveRecord::Base
   end
 
 
+
 end
